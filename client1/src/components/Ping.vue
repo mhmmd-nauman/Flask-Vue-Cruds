f8fb34af6002771a9ef283e351cf1f37846cb19e
Dr. Muhammad Nauman <mhmmd.nauman@gmail.com>
2022-07-31 00:40:10 +0500

    flask vue cruds

---
+++ client1/src/components/Ping.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="container">
+    <button type="button" class="btn btn-primary">Pong</button>
+  </div>
+</template>
---
+++ client1/src/router/index.js
@@ -0,0 +1,30 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Ping from '../components/Ping.vue';
+import 'bootstrap/dist/css/bootstrap.css';
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/ping',
+    name: 'Ping',
+    component: Ping,
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  },
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
